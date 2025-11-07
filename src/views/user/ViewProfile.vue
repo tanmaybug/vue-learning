1810b7c50ca92e1deab9ab430139d8e738cf84a6
tanmaybug <tanmay.bug@gmail.com>
2025-11-07 16:33:30 +0530

    profile page update

--- src/views/user/ViewProfile.vue
+++ src/views/user/ViewProfile.vue
@@ -1,7 +1,43 @@
 <template>
   <section class="text-center primary-color">
     <h2>View Profile {{userId}}</h2>
-    <p>We are passionate developers building awesome stuff!</p>
+    <br/>
+
+    <div class="row">
+      <div class="col-md-12">
+        <table class="table table-bordered table-striped">
+          <tr><th colspan="2">Applicant Details</th></tr>
+          <tr>
+            <th>Name</th>
+            <td>Tanmay</td>
+          </tr>
+          <tr>
+            <th>Phone</th>
+            <td>1234567890</td>
+          </tr>
+          <tr>
+            <th>Email</th>
+            <td>test@gamil.com</td>
+          </tr>
+          <tr>
+            <th>PIN</th>
+            <td>71101</td>
+          </tr>
+          <tr>
+            <th>State</th>
+            <td>West Bengal</td>
+          </tr>
+          <tr>
+            <th>District</th>
+            <td>Howrah</td>
+          </tr>
+          <tr>
+            <th>Address</th>
+            <td>23 howrah</td>
+          </tr>
+        </table>
+      </div>
+    </div>
   </section>
 </template>
 
